c7462e20b0ac4d8d78118fe65816d264a89a9c68
rui shan <91321880+Shanrui03@users.noreply.github.com>
2022-06-01 22:24:04 +0800

    22.23

--- Assets/Script/GitHubTest.cs
+++ Assets/Script/GitHubTest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GitHubTest : MonoBehaviour
 {
-    private string str = "Test";
+    private string str = "Test01";
     // Start is called before the first frame update
     void Start()
     {
